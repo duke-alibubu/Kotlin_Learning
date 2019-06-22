1406f1cdfbef6325c802ae6bf3193fe8fa635636
Trinh Dung <c170017@e.ntu.edu.sg>
2019-06-22 22:30:34 +0800

    Inlines

--- src/Aquarium5/Aquarium5.kt
+++ src/Aquarium5/Aquarium5.kt
@@ -10,12 +10,16 @@ fun main(args: Array<String>)
 fun fishExamples(){
 	val fish = Fish("splashy");
 	
+	
+	//a problem with this. Every time we call this func, a new lambda object is created. 
 	myWith (fish.name){ 
 		println(capitalize());
 	}
 }
 
-fun myWith(name: String, block: String.() -> Unit){
+//inline: A promise that everytime myWith is called, it will actually transform the source code to inLine, the function.
+//That is, the compiler will change the code to replace the Lambda with the instructions inside the Lambda --> zero overhead. 
+inline fun myWith(name: String, block: String.() -> Unit){
 	//block is now an extension function on a string object and we can apply it to a string
 	
 	name.block();
